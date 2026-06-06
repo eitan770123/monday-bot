35680f96fec38860f90bd4663cadafa82c95a34c
eitan770123 <86793209+eitan770123@users.noreply.github.com>
2026-06-07 02:36:52 +0300

    Refactor server.js to improve color change logic

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// מפת הצבעים לפי הסדר המדויק שביקשת (1-3 המוגדרים, 4-8 אקראיים יפים, 9 הפתעה)
 const COLOR_MAP = {
     '1': '#ffeb3b',    // 1. צהוב
     '2': '#ff0000',    // 2. אדום
@@ -24,11 +23,9 @@ app.get('/change-color', async (req, res) => {
         return res.send("id_list_message=t-מקש שגוי. אנא לחצו על מקש בין 1 ל 9&go_to_folder=/1");
     }
 
-    // הטוקן המקורי והחזק שתפסת בעצמך מהאתר
     const token = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc5OTRiNGYzMTU2MzJiMjk3NzAwNmQ5M2U5NGIyYWNiZTMwNWZlNDYiLCJ0eXAiOiJKV1QifQ.eyJwcm92aWRlcl9pZCI6ImFub255bW91cyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9tYWtlcnMtcHJvZCIsImF1ZCI6Im1ha2Vycy1wcm9kIiwiYXV0aF90aW1lIjoxNzgwNTc3OTg4LCJ1c2VyX2lkIjoidkhyU0Jjd09ob1FodXdWMmZ5dTRrQUd5cnNtMSIsInN1YiI6InZIclNCY3dPaG9RaHV3VjJmeXU0a0FHeXJzbTEiLCJpYXQiOjE3ODA1Nzc5ODgsImV4cCI6MTc4MDU4MTU4OCwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6e30sInNpZ25faW5fcHJvdmlkZXIiOiJhbm9ueW1vdXMifX0.Ii-yTb5uX4StCXfVKZVtqsxqk2D85cAsVq89xuHOLiKMU2hQpAbwwLLirgPSZP2Dt56dwZqd79PRPOoMi_-7tsKsToHMrYUhsvxcSOoA755U_crBLIYO-xMqvLfd2VDw85aj6pvUfq--s-UlOTvmrkJztjJ52yEMj0HrKZueK5ZRtDfJZmf25s0b05LnFsViUD1DSz6mMHgBTR27HNSNjnUVj7te_VPXd2ETLRVoKd-hkpuz4vkD5hia4RlY9PaVJ-A55w2zrrZZaRxLp9xZV3TW6hhYMLlqf6jWFQEWYCQclZELUdv536ViyDMvKPYeCW4G23ElILDd7ZQ74ou5og";
 
     try {
-        // שליחת הפקודה ישירות ל-API של מאנדיי עם הטוקן שלך
         await axios.post('https://mondaysign.com/api/sendSmartSignCommand', 
             { color: targetColor }, 
             {
